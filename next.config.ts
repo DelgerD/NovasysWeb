4595bbea33ba625658cad27f3d4cd685a9fa2417
Nasaa <nasaa.devil@gmail.com>
2026-01-15 15:31:08 +0800

    config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
+import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  webpack: (config) => {
+    // "@/..." path alias-г src folder-тэй холбох
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
 };
-
 
 export default nextConfig;
